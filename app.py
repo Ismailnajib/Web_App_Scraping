71a3204475599de16fae49e8ac6b81c2d519929f
Ismail Najib <161603302+Ismailnajib@users.noreply.github.com>
2025-01-03 19:19:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import subprocess
 
 def scrape_website(website):
  
-        chrome_driver_path = "chromedriver"
+        chrome_driver_path = "./chromedriver.exe"
        
         options = Options()
         driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options) 
